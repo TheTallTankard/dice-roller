ed3a97c4b34a10275e15f93eb6133906af5be54e
Tyler Jass <jasstsg@gmail.com>
2022-12-09 15:39:53 -0500

    Added header, updated title and favicon

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../styles/App.css';
+import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
 
@@ -15,6 +16,7 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
+        <Header></Header>
         <Body></Body>
         <Footer></Footer>
       </div>
---
+++ src/scripts/components/Header.js
@@ -0,0 +1,13 @@
+import '../../styles/Header.css';
+
+function Header () {
+    return (
+        <div className="Header">
+            <h2>
+                D&D 5th Edition Dice Roller
+            </h2>
+        </div>
+    )
+}
+
+export default Header;
